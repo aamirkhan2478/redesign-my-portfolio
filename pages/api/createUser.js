138131b42a53908e207a4e2b6584dedb87ee5ab2
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-02-22 14:36:42 +0500

    add register api

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,14 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectDb from "@/middlewares/mongoose";
 import User from "@/models/userModel";
+import Joi from "joi";
+
+const signupSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).max(10).required(),
+});
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    console.log(req.body);
     const { name, email, password } = req.body;
-    let user = new User({name, email, password});
-    await user.save();
-    res.status(200).json({ success: "success" });
+    const { error } = signupSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(400).send(error.details);
+    }
+    try {
+      const checkEmail = await User.findOne({ email });
+      if (checkEmail) {
+        return res.status(400).json({ error: "Email already exists" });
+      }
+      let user = new User({ name, email, password });
+      await user.save();
+      res.status(200).json({ success: "success" });
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send("Server error");
+    }
   } else {
     res.status(400).json({ error: "This method is not allowed" });
   }
