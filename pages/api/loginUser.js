4ef990543ac262d8200954dc1179ef24e9503ec9
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-02-22 19:37:11 +0500

    add login api functionality

--- pages/api/loginUser.js
+++ pages/api/loginUser.js
@@ -1,19 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectDb from "@/middlewares/mongoose";
 import User from "@/models/userModel";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 const handler = async (req, res) => {
   if (req.method === "POST") {
-    const { name, email, password } = req.body;
-    
+    const { email, password } = req.body;
+
     try {
-      const checkEmail = await User.findOne({ email });
-      if (checkEmail) {
-        return res.status(400).json({ error: "Email already exists" });
+      const user = await User.findOne({ email });
+      if (user) {
+        const isMatch = await compare(password, user.password);
+        if (email === user.email) {
+          if (!isMatch) {
+            return res.status(400).json({
+              success: false,
+              error: "Invalid Credentials",
+            });
+          }
+          var token = sign(
+            { email: user.email, name: user.name },
+            process.env.JWT_TOKEN_SECRET,
+            { expiresIn: "2d" }
+          );
+          return res.status(200).json({
+            success: true,
+            token,
+          });
+        } else {
+          return res.status(400).json({
+            success: false,
+            error: "Invalid Credentials",
+          });
+        }
+      } else {
+        return res.status(400).json({
+          success: false,
+          error: "No User Found",
+        });
       }
-      let user = new User({ name, email, password });
-      await user.save();
-      res.status(200).json({ success: "success" });
     } catch (err) {
       console.log(err.message);
       return res.status(500).send("Server error");
